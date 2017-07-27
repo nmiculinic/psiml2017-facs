6fe2e0043268bb70452135fc5bc55a19c2771a8f
Neven Miculinic <neven.miculinic@gmail.com>
2017-07-27 23:15:28 +0200

    Implemented data generation

--- dataset.py
+++ dataset.py
@@ -8,9 +8,15 @@ import argparse
 import sys
 from glob import glob
 import logging
+import random
 
 
 pp = PrettyPrinter()
+
+class Faces10K:
+    def __init__(self, root_path, train_test_split=0.3):
+        self.root_path = root_path
+        self.train_test_split = train_test_split
 
 def faces_10k_dataset(root_path):
     wb = load_workbook(
@@ -35,54 +41,10 @@ def faces_10k_dataset(root_path):
     return data 
 
 
-def cohn_kanade_dataset(root_path, max_num=1000000):
-    rootdir_image = os.path.join(root_path, "cohn-kanade-images")
-    rootdir_facs = os.path.join(root_path, "FACS")
-    rootdir_emotions = os.path.join(root_path, "Emotion")
-    rootdir_landmarks = os.path.join(root_path, "Landmarks")
-
-    file_list_image = [y for x in os.walk(rootdir_image) for y in glob(os.path.join(x[0], '*.png'))]
-
-    sol = []
-    for image_fname in tqdm(file_list_image[:max_num]):
-        try:
-            basename, ext = os.path.splitext(os.path.basename(image_fname))
-            sub, seq, sequence_num = basename.split('_')
-
-            attrs = {
-                'image': Image.open(image_fname).copy()
-            }
-
-            facs_fname = os.path.join(rootdir_facs, sub, seq, "%s_%s_%s_facs.txt" % (sub, seq, sequence_num))
-            if os.path.exists(facs_fname):
-                attrs['facs'] = np.loadtxt(facs_fname)
-            else:
-                pass
-                # print(facs_fname, "doesn't exist; skipping!")
-
-            landmarks_fname = os.path.join(rootdir_landmarks, sub, seq, "%s_%s_%s_landmarks.txt" % (sub, seq, sequence_num))
-            if os.path.exists(landmarks_fname):
-                attrs['landmarks'] = np.loadtxt(landmarks_fname)
-            else:
-                print(landmarks_fname, "doesn't exist; skipping!")
-                raise ValueError("Landmark must exist!")
-
-            sol.append(attrs)
-        except Exception:
-            print("ERROR", image_fname)
-            raise
-    logging.info(
-            "CK has %d images, %d has FACS info", 
-            len(sol),
-            len(list(filter(lambda x: 'facs' in x.keys(), sol)))
-    )
-    return sol
-
-def resize_picture(self, fname, lname):
-    img = Image.open(fname)
-    landmarks = np.loadtxt(lname)
+def resize_datapoint(datapoint, PICTURE_SIZE):
+    img = datapoint['image'].convert("RGB")
+    landmarks = datapoint['landmarks']
     width, height = img.size[:2]
-    PICTURE_SIZE = 128
 
     if height>width:
         resized_height = PICTURE_SIZE
@@ -108,9 +70,81 @@ def resize_picture(self, fname, lname):
     offset_image = np.zeros((PICTURE_SIZE,PICTURE_SIZE,3),np.uint8)
     offset_image= Image.fromarray(offset_image)
     offset_image.paste(img,(x_offset,y_offset))
-    #offset_image.save(imOut)
-    #np.savetxt(landOut,landmarks)
-    return offset_image, landmarks
+
+    datapoint['image'] = offset_image.convert("L")
+    datapoint['landmarks'] = landmarks
+    return datapoint
+    
+class CohnKanade:
+    def __init__(self, root_path, picture_size=128, train_test_split=0.7):
+        self.root_path = root_path
+        self.rootdir_image = os.path.join(root_path, "cohn-kanade-images")
+        self.rootdir_facs = os.path.join(root_path, "FACS")
+        self.rootdir_emotions = os.path.join(root_path, "Emotion")
+        self.rootdir_landmarks = os.path.join(root_path, "Landmarks")
+        file_list_image = [y for x in os.walk(self.rootdir_image) for y in glob(os.path.join(x[0], '*.png'))]
+
+        split_point = int(len(file_list_image) * train_test_split)
+        self.train_file_list = file_list_image[:split_point]
+        self.test_file_list = file_list_image[split_point:]
+        self.picture_size = picture_size
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Train set size %d", len(self.train_file_list))
+        self.logger.info("Test set size %d",  len(self.test_file_list))
+
+    def datapoint_for_file(self, image_fname):
+        basename, ext = os.path.splitext(os.path.basename(image_fname))
+        sub, seq, sequence_num = basename.split('_')
+
+        attrs = {
+            'image': Image.open(image_fname).copy()
+        }
+
+        facs_fname = os.path.join(self.rootdir_facs, sub, seq, "%s_%s_%s_facs.txt" % (sub, seq, sequence_num))
+        if os.path.exists(facs_fname):
+            attrs['facs'] = np.loadtxt(facs_fname)
+        else:
+            pass
+            # print(facs_fname, "doesn't exist; skipping!")
+
+        landmarks_fname = os.path.join(self.rootdir_landmarks, sub, seq, "%s_%s_%s_landmarks.txt" % (sub, seq, sequence_num))
+        if os.path.exists(landmarks_fname):
+            attrs['landmarks'] = np.loadtxt(landmarks_fname)
+        else:
+            print(landmarks_fname, "doesn't exist; skipping!")
+            raise ValueError("Landmark must exist!")
+        return attrs
+
+    def datapoint_generator(self, flist, batch_size):
+        curr_batch_x = []
+        curr_batch_y = []
+        flist = flist[:]
+        while True:
+            random.shuffle(flist)
+            for image_fname in flist:
+                try:
+                    dp = self.datapoint_for_file(image_fname)
+                    dp = resize_datapoint(dp, self.picture_size)
+                    curr_batch_x.append(np.array(dp['image'])[:,:,None])
+                    curr_batch_y.append(dp['landmarks'])
+                    if len(curr_batch_x) == batch_size:
+                        yield (
+                            np.array(curr_batch_x),
+                            np.array(curr_batch_y)
+                        )
+                        curr_batch_x = []
+                        curr_batch_y = [] 
+                except Exception as ex:
+                    self.logger.error("In %s %s happend", image_fname, ex)
+
+    def train_generator(self, batch_size):
+        for x in self.datapoint_generator(self.train_file_list, batch_size):
+            yield x
+
+    def test_generator(self, batch_size):
+        for x in self.datapoint_generator(self.test_file_list, batch_size):
+            yield x
 
 if __name__ == "__main__":
     cohn_kanade_dataset(sys.argv[1])
--- train.py
+++ train.py
@@ -18,10 +18,11 @@ haikunator = Haikunator()
 
 class LandmarkPreview(Callback):
     def __init__(self, out_dir,
-                 batch_size=10,):
+                 batch_size=10, validation_gen=None):
         super().__init__()
         self.out_dir = out_dir
         self.batch_size = batch_size
+        self.validation_gen = validation_gen
         os.makedirs(out_dir, exist_ok=True)
 
     def draw_landmarks(self, image, landmarks, r=1, fill_color=(255,0,0,100)):
@@ -33,54 +34,30 @@ class LandmarkPreview(Callback):
     def on_epoch_end(self, epoch, logs=None):
         logs = logs or {}
         if self.validation_data:
-            val_size = self.validation_data[0].shape[0]
-            samples = np.random.choice(np.arange(val_size), self.batch_size)
+            data = self.validation_data 
+        else:
+            x, y = next(self.validation_gen)
+            data = [x, y, None]
 
-            x_test = self.validation_data[0][samples]
-            y_targets = self.validation_data[1][samples]
-            y_preds = self.model.predict(x_test)
+        val_size = data[0].shape[0]
+        samples = np.random.choice(np.arange(val_size), self.batch_size)
+        x_test = data[0][samples]
+        y_targets = data[1][samples]
+        y_preds = self.model.predict(x_test)
 
-            for i, x, y_true, y_pred in zip(np.arange(val_size), x_test, y_targets, y_preds):
-                img = Image.fromarray(np.squeeze(x, axis=2) * 255.0).convert("RGBA")
-                # img.save(os.path.join(self.out_dir, "epoh_%2d_%2d.png" % (epoch, i)))
-                self.draw_landmarks(img, y_true, r=2, fill_color=(255,0,0,100))
-                self.draw_landmarks(img, y_pred, r=2, fill_color=(0,255,0,100))
-                img.save(os.path.join(self.out_dir, "epoh_%2d_%2d_marker.png" % (epoch, i)))
-
-def train_model():
-    pass
-
-if __name__ == "__main__":
-    log_fmt = '[%(levelname)s] %(name)s: %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+        for i, x, y_true, y_pred in zip(np.arange(val_size), x_test, y_targets, y_preds):
+            img = Image.fromarray(np.squeeze(x, axis=2) * 255.0).convert("RGBA")
+            self.draw_landmarks(img, y_true, r=2, fill_color=(255,0,0,100))
+            self.draw_landmarks(img, y_pred, r=2, fill_color=(0,255,0,100))
+            img.save(os.path.join(self.out_dir, "epoh_%2d_%2d_marker.png" % (epoch, i)))
 
 
-    name = haikunator.haikunate() 
-    logger = logging.getLogger(name)
-    os.makedirs(os.path.join('.', 'logs', name), exist_ok=True)
-    fh = FileHandler(os.path.join('.', 'logs', name, 'log.txt'))
-    fh.setLevel(logging.INFO)
-    logger.addHandler(fh)
-
-    logger.info("Started data loading.")
-    sol = dataset.cohn_kanade_dataset(sys.argv[1])
-    for x in sol:
-        x['image'] = np.array(x['image'].convert("L"))
-        
-    sol = list(filter(lambda x:x['image'].shape == (490, 640), sol))
-
-    x_train = [x['image'] for x in sol]
-    x_train = np.array(x_train) / 255.0
-    x_train = x_train[:, :, :, None]
-
-    y_train = np.array([x['landmarks'] for x in sol])
-    
-    logger.info("x_train.shape %s, y_train.shape %s", str(x_train.shape), str(y_train.shape))
+def complex_model(input_shape):
     model = Sequential()
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
                      padding='SAME',
-                     input_shape=x_train[0].shape))
+                     input_shape=input_shape))
     model.add(BatchNormalization())
     model.add(Conv2D(32, kernel_size=(3, 3),
                      activation='relu',
@@ -109,30 +86,72 @@ if __name__ == "__main__":
     model.compile(loss=keras.losses.mean_squared_error,
                   optimizer='adam',
     )
+    return model
 
+
+def simple_model(input_shape):
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     padding='SAME',
+                     input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D())
+    model.add(Conv2D(64, kernel_size=(3, 3),
+                     activation='relu',
+                     padding='SAME',
+    ))
+    model.add(MaxPooling2D())
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(68 * 2, activation='relu'))
+    model.add(Reshape((68, 2)))
+    model.compile(loss=keras.losses.mean_squared_error,
+                  optimizer='adam',
+    )
+    return model
+
+
+if __name__ == "__main__":
+    log_fmt = '[%(levelname)s] %(name)s: %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    name = haikunator.haikunate() 
+    logger = logging.getLogger(name)
+    os.makedirs(os.path.join('.', 'logs', name), exist_ok=True)
+    fh = FileHandler(os.path.join('.', 'logs', name, 'log.txt'))
+    fh.setLevel(logging.INFO)
+    logger.addHandler(fh)
+
+    logger.info("Started data loading.")
+    model = simple_model((128, 128, 1))
     logger.info("Model summary\n%s", model.summary())
     
+    dataset = dataset.CohnKanade(sys.argv[1])
     checkpointer = ModelCheckpoint(
              os.path.join('.', 'logs', name, 'checkpoint'),
              verbose=0, 
              save_best_only=True
     )
-
     tensorboard = TensorBoard(
         os.path.join('.', 'logs', name),
         # write_grads=True,
         # histogram_freq=3
     )
-
     landmarks = LandmarkPreview(
-        os.path.join('.', 'logs', name, 'pics')
+        os.path.join('.', 'logs', name, 'pics'),
+        validation_gen = dataset.test_generator(10)
     )
 
-    model.fit(x_train, y_train,
-        batch_size=16,
-        epochs=20,
+    model.fit_generator(
+        generator=dataset.train_generator(2),
+        steps_per_epoch=5,
+        epochs=100,
         verbose=1,
-        validation_split=0.1,
-        callbacks = [checkpointer, tensorboard, landmarks]
+        validation_data=dataset.test_generator(2),
+        validation_steps=1,
+        callbacks = [checkpointer, tensorboard, landmarks],
+        # max_queue_size = 100
     )
     model.save(os.path.join('.', 'models', name + "_model.h5"))
