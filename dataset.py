322e1728f41d9572324ba5c78addb0c4b212c2fa
Ponfrit <stefanvukovic44@gmail.com>
2017-07-28 13:48:56 +0200

    Rotation of images

--- dataset.py
+++ dataset.py
@@ -41,50 +41,61 @@ def faces_10k_dataset(root_path):
     return data 
 
 
-def resize_datapoint(datapoint, PICTURE_SIZE = 132, SMALLER_SIZE = 4):
+def resize_datapoint(datapoint, picture_size = 128, crop_window = 4, max_angle = 15.):
+    picture_size += crop_window
     img = datapoint['image'].convert("RGB")
     landmarks = datapoint['landmarks']
     width, height = img.size[:2]
 
     if height>width:
-        resized_height = PICTURE_SIZE
-        resized_width = round(PICTURE_SIZE*width/height)
-        x_offset=round((PICTURE_SIZE-resized_width)/2)
+        resized_height = picture_size
+        resized_width = round(picture_size*width/height)
+        x_offset=round((picture_size-resized_width)/2)
         y_offset=0
         assert x_offset >= 0
-        landmarks[:, 0] = landmarks[:, 0] * (PICTURE_SIZE/height) + x_offset
-        landmarks[:, 1] = landmarks[:, 1] * (PICTURE_SIZE/height)
+        landmarks[:, 0] = landmarks[:, 0] * (picture_size/height) + x_offset
+        landmarks[:, 1] = landmarks[:, 1] * (picture_size/height)
     elif height<width:
-        resized_height = round(PICTURE_SIZE*height/width)
-        resized_width = PICTURE_SIZE
+        resized_height = round(picture_size*height/width)
+        resized_width = picture_size
         x_offset=0
-        y_offset=round((PICTURE_SIZE-resized_height)/2)
+        y_offset=round((picture_size-resized_height)/2)
         assert y_offset >= 0
-        landmarks[:, 0] = landmarks[:, 0] * (PICTURE_SIZE/width)
-        landmarks[:, 1] = landmarks[:, 1] * (PICTURE_SIZE/width) + y_offset
+        landmarks[:, 0] = landmarks[:, 0] * (picture_size/width)
+        landmarks[:, 1] = landmarks[:, 1] * (picture_size/width) + y_offset
     else:
-        resized_height = PICTURE_SIZE
-        resized_width = PICTURE_SIZE
+        resized_height = picture_size
+        resized_width = picture_size
         x_offset=y_offset=0
-        landmarks[:, 0] = landmarks[:, 0] * (PICTURE_SIZE/width)
-        landmarks[:, 1] = landmarks[:, 1] * (PICTURE_SIZE/height)
+        landmarks[:, 0] = landmarks[:, 0] * (picture_size/width)
+        landmarks[:, 1] = landmarks[:, 1] * (picture_size/height)
     img = img.resize((resized_width,resized_height),Image.ANTIALIAS)
 
-    crop_x = int(random.random()*SMALLER_SIZE)
+    crop_x = int(random.random()*crop_window)
     crop_y = int(random.random()*4)
     mirror = int(random.random())
     img = img.crop(
         (crop_x, crop_y,
-        PICTURE_SIZE-SMALLER_SIZE+crop_x, PICTURE_SIZE-SMALLER_SIZE+crop_y)
+        picture_size-crop_window+crop_x, picture_size-crop_window+crop_y)
         )
     if mirror == 1:
         img = img.transpose(Image.FLIP_LEFT_RIGHT)
-        landmarks[:, 0] = PICTURE_SIZE-SMALLER_SIZE - landmarks[:,0]
-        landmarks[:, 1] = PICTURE_SIZE-SMALLER_SIZE - landmarks[:,1]
+        landmarks[:, 0] = picture_size-crop_window - landmarks[:,0]
+        landmarks[:, 1] = picture_size-crop_window - landmarks[:,1]
 
-    offset_image = np.zeros((PICTURE_SIZE-SMALLER_SIZE,PICTURE_SIZE-SMALLER_SIZE,3),np.uint8)
+    offset_image = np.zeros((picture_size-crop_window,picture_size-crop_window,3),np.uint8)
     offset_image = Image.fromarray(offset_image)
     offset_image.paste(img,(x_offset,y_offset))
+
+    angle = -max_angle + 2*max_angle*random.random()
+    theta = np.radians(-angle)
+    offset_image = offset_image.rotate(angle)
+    rotMatrix = np.array([[np.cos(theta),-np.sin(theta)],[np.sin(theta),np.cos(theta)]])
+    landmarks[:, 0] -= (picture_size-crop_window)/2
+    landmarks[:, 1] -= (picture_size-crop_window)/2
+    landmarks = (np.dot(rotMatrix,landmarks.T).T)
+    landmarks[:, 0] += (picture_size-crop_window)/2
+    landmarks[:, 1] += (picture_size-crop_window)/2
 
     datapoint['image'] = offset_image.convert("L")
     datapoint['landmarks'] = landmarks
