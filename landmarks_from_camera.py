961f2be33c6bdafd10e597ba43f5dde75c32e503
Neven Miculinic <neven.miculinic@gmail.com>
2017-07-28 22:50:43 +0200

    update

--- landmarks_from_camera.py
+++ landmarks_from_camera.py
@@ -4,16 +4,16 @@ import cv2
 import time
 import dataset
 from PIL import Image, ImageDraw
-# from keras.models import load_model
+from keras.models import load_model
 
 args = argparse.ArgumentParser()
 args.add_argument("model_path")
 args = args.parse_args()
 
-# model = load_model(args.model_path)
-# print(model.inputs)
-# dim = int(model.input.shape[1])
-dim = 160
+model = load_model(args.model_path)
+print(model.inputs)
+dim = int(model.input.shape[1])
+# dim = 160
 
 def draw_landmarks(image, landmarks, r=1, fill_color=(255,0,0,100)):
     draw = ImageDraw.Draw(image)
@@ -38,22 +38,21 @@ while(True):
     dp = dataset.resize_mirror_datapoint(dp, dim, False)
     img = dp['image']
 
-#         landmark = model.predict(np.array(img.convert("L"))[None, :, :, None] / 255.0)
     landmark = np.array([[
         [0,0],
         [50,50],
         [100,50]
     ]])
+    landmark = model.predict(np.array(img.convert("L"))[None, :, :, None] / 255.0)
     landmark = np.squeeze(landmark, axis=0)
     
     h, w = img.size
-    img = img.resize((2*h, 2*w))
+    img = img.resize((2*h, 2*w)).convert("L")
     landmark *= 2
-    
-    draw_landmarks(img, landmark, r=1)        
+    draw_landmarks(img, landmark, r=2)        
     
     # Convert RGB to BGR 
-    img = np.array(img) 
+    img = np.array(img.convert("RGB")) 
     img = img[:, :, ::-1].copy() 
     cv2.imshow('frame', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
